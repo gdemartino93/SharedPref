2f089534f63a85e9996cacf9ad4234af1bd5843a
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-12-29 23:40:18 +0100

    shared preferences on switch

--- app/src/main/java/com/gianluca/sharedpref/MainActivity.java
+++ app/src/main/java/com/gianluca/sharedpref/MainActivity.java
@@ -2,13 +2,29 @@ package com.gianluca.sharedpref;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+    Switch swNotifications;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swNotifications = findViewById(R.id.swNotifications);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isNotificationEnabled = sharedPreferences.getBoolean("notifications",false);
+        swNotifications.setChecked(isNotificationEnabled);
+        swNotifications.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                sharedPreferences.edit().putBoolean("notifications",b).apply();
+            }
+        });
     }
 }
